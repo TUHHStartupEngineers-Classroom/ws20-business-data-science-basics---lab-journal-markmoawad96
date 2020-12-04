b2a5ff899cedb4330932d421749bb8876dec4d2e
markmoawad96 <74685053+markmoawad96@users.noreply.github.com>
2020-12-04 19:24:03 +0100

    Journal_add_challenge_3

--- Country_list_API_challenge.R
+++ Country_list_API_challenge.R
@@ -6,4 +6,14 @@ my_apikey <- Sys.getenv("pwd")
 url <- modify_url(url = "http://dataservice.accuweather.com", path = glue("/locations/v1/topcities/100?apikey={my_apikey}&language=en-us&details=false"))
 resp <- GET(url)
 country_list <- resp %>% .$content %>% rawToChar() %>% fromJSON()
-head(country_list, 10)
+City <- country_list$EnglishName
+Country <- country_list$Country$EnglishName
+Region <- country_list$Region$EnglishName
+Longitude <- country_list$GeoPosition$Longitude
+Latitude <- country_list$GeoPosition$Latitude
+Timezone <- country_list$TimeZone$Name
+#optional to add GMT_Offset, it's only removed so that printing fits the screen
+#GMT_Offset <- country_list$TimeZone$GmtOffset
+country_list_as_df <- data.frame(City, Country, Region,
+                                  Longitude, Latitude, Timezone)
+head(country_list_as_df, 10)
--- Web_Scraping_challenge.R
+++ Web_Scraping_challenge.R
@@ -1,12 +1,10 @@
-# 1.0 LIBRARIES ----
-
 library(tidyverse) # Main Package - Loads dplyr, purrr, etc.
 library(rvest)     # HTML Hacking & Web Scraping
 library(xopen)     # Quickly opening URLs
 library(jsonlite)  # converts JSON files to R objects
 library(glue)      # concatenate strings
 library(stringi)   # character string/text processing
-
+library(ggplot2)
 url_home <- "https://www.rosebikes.de/fahrr%C3%A4der/mtb"
 
 html_home <- read_html(url_home)
@@ -42,3 +40,8 @@ bike_price_tbl_challenge <- html_home %>%
 bike_name_price_tbl <- left_join(bike_name_tbl_challenge, bike_price_tbl_challenge) %>%
   select(-position)
 bike_name_price_tbl
+ggplot(bike_name_price_tbl, aes(x = name, y = price_in_EUR, color = name)) +
+  geom_col() +
+  expand_limits(x = 0, y = 0) +
+  labs(title = "Bike Model vs Price") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
